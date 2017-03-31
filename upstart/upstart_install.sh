4686bbe3fc0523ec635693b6d7f703ecc8b6b489
ipa-cob4-5 <cob4-5@ipa.fhg.de>
2017-03-31 13:17:39 +0200

    robot test

--- upstart/upstart_install.sh
+++ upstart/upstart_install.sh
@@ -4,22 +4,25 @@ robot_name="${HOSTNAME//-b1}"
 
 sudo apt-get install ros-indigo-robot-upstart
 sudo apt-get install tmux
+sudo apt-get install nmap
 
-sudo cp /u/robot/git/setup_cob4/upstart/cob.conf /etc/init/cob.conf
-sudo cp /u/robot/git/setup_cob4/upstart/cob-start /usr/sbin/cob-start
+sudo cp /tmp/setup_cob4/upstart/cob-start /usr/sbin/cob-start
 sudo sed -i "s/myrobotname/$robot_name/g" /usr/sbin/cob-start
 sudo sed -i "s/mydistro/$ROS_DISTRO/g" /usr/sbin/cob-start
 sudo sed -i "s/myrobot/$ROBOT/g" /usr/sbin/cob-start
 sudo sed -i "s/myuser/robot/g" /usr/sbin/cob-start
 echo "%users ALL=NOPASSWD:/usr/sbin/cob-start" | sudo tee -a /etc/sudoers
 
-sudo cp /u/robot/git/setup_cob4/scripts/cob-stop /usr/sbin/cob-stop
+sudo cp /tmp/setup_cob4/scripts/cob-stop /usr/sbin/cob-stop
 sudo sed -i "s/myrobotname/$robot_name/g" /usr/sbin/cob-stop
 sudo sed -i "s/myuser/msh/g" /usr/sbin/cob-stop
 echo "%users ALL=NOPASSWD:/usr/sbin/cob-stop" | sudo tee -a /etc/sudoers
 
-sudo cp /u/robot/git/setup_cob4/scripts/cob-command /usr/sbin/cob-command
+sudo cp /tmp/setup_cob4/scripts/cob-command /usr/sbin/cob-command
 sudo echo "%users ALL=NOPASSWD:/usr/sbin/cob-command" | sudo tee -a /etc/sudoers
+
+sudo cp /tmp/setup_cob4/scripts/tmux_cob /usr/sbin/tmux_cob
+sudo echo "%users ALL=NOPASSWD:/usr/sbin/tmux_cob" | sudo tee -a /etc/sudoers
 
 camera_client_list="
 $robot_name-t2
@@ -31,6 +34,6 @@ for client in $camera_client_list; do
         echo "Executing on $client"
         echo "-------------------------------------------"
         echo ""
-        ssh $client "sudo cp /u/robot/git/setup_cob4/upstart/check_cameras.sh /etc/init.d/check_cameras.sh"
-        ssh $client "sudo update-rc.d check_cameras.sh defaults"
+        #ssh $client "sudo cp /u/robot/git/setup_cob4/upstart/check_cameras.sh /etc/init.d/check_cameras.sh"
+        #ssh $client "sudo update-rc.d check_cameras.sh defaults"
 done
